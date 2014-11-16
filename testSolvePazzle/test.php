714e1bb318efbd9db815bbf010be6d2423280aa6
stad1237 <stad1237@yahoo.co.jp>
2014-11-17 05:32:34 +0900

    add sample

---
+++ testSolvePazzle/test.php
@@ -0,0 +1,23 @@
+<?php
+
+require('solve_main.php');
+// require('aa.php');
+
+
+class StackTest extends PHPUnit_Framework_TestCase
+{
+    public function testPushAndPop()
+    {
+        $stack = array();
+        $this->assertEquals(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertEquals(1, count($stack));
+
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
+    }
+}
+
+
